7e30d06e13e23db334b6a811ec12b61cc27135ce
Mani Movassagh <mani.mgh@gmail.com>
2024-10-18 04:23:53 +0200

    Redis clone

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -1,102 +1,102 @@
 package commands
 
 import (
-    "bufio"
-    "log"
-    "net"
-    "strings"
+	"bufio"
+	"log"
+	"net"
+	"strings"
 
-    "github.com/manimovassagh/Godis/internal/aof"
-    "github.com/manimovassagh/Godis/internal/datastore"
-    "github.com/manimovassagh/Godis/internal/protocol"
+	"github.com/manimovassagh/Godis/internal/aof"
+	"github.com/manimovassagh/Godis/internal/datastore"
+	"github.com/manimovassagh/Godis/internal/protocol"
 )
 
 type Client struct {
-    conn      net.Conn
-    reader    *bufio.Reader
-    datastore *datastore.DataStore
-    aof       *aof.AOFHandler
+	conn      net.Conn
+	reader    *bufio.Reader
+	datastore *datastore.DataStore
+	aof       *aof.AOFHandler
 }
 
 func NewClient(conn net.Conn) *Client {
-    return &Client{
-        conn:      conn,
-        reader:    bufio.NewReader(conn),
-        datastore: datastore.GetDataStore(),
-        aof:       aof.GetAOFHandler(),
-    }
+	return &Client{
+		conn:      conn,
+		reader:    bufio.NewReader(conn),
+		datastore: datastore.GetDataStore(),
+		aof:       aof.GetAOFHandler(),
+	}
 }
 
 func (c *Client) Handle() {
-    defer c.conn.Close()
-    clientAddr := c.conn.RemoteAddr().String()
-    log.Printf("Client connected: %s", clientAddr)
+	defer c.conn.Close()
+	clientAddr := c.conn.RemoteAddr().String()
+	log.Printf("Client connected: %s", clientAddr)
 
-    for {
-        args, err := protocol.ParseRequest(c.reader)
-        if err != nil {
-            protocol.WriteError(c.conn, "ERR "+err.Error())
-            return
-        }
-        if len(args) == 0 {
-            protocol.WriteError(c.conn, "ERR empty command")
-            continue
-        }
-        cmd := strings.ToUpper(args[0])
-        switch cmd {
-        case "PING":
-            c.ping(args)
-        case "ECHO":
-            c.echo(args)
-        case "SET":
-            c.set(args)
-        case "GET":
-            c.get(args)
-        default:
-            protocol.WriteError(c.conn, "ERR unknown command '"+cmd+"'")
-        }
-    }
+	for {
+		args, err := protocol.ParseRequest(c.reader)
+		if err != nil {
+			protocol.WriteError(c.conn, "ERR "+err.Error())
+			return
+		}
+		if len(args) == 0 {
+			protocol.WriteError(c.conn, "ERR empty command")
+			continue
+		}
+		cmd := strings.ToUpper(args[0])
+		switch cmd {
+		case "PING":
+			c.ping(args)
+		case "ECHO":
+			c.echo(args)
+		case "SET":
+			c.set(args)
+		case "GET":
+			c.get(args)
+		default:
+			protocol.WriteError(c.conn, "ERR unknown command '"+cmd+"'")
+		}
+	}
 }
 
 func (c *Client) ping(args []string) {
-    var response string
-    if len(args) > 1 {
-        response = args[1]
-    } else {
-        response = "PONG"
-    }
-    protocol.WriteSimpleString(c.conn, response)
+	var response string
+	if len(args) > 1 {
+		response = args[1]
+	} else {
+		response = "PONG"
+	}
+	protocol.WriteSimpleString(c.conn, response)
 }
 
 func (c *Client) echo(args []string) {
-    if len(args) != 2 {
-        protocol.WriteError(c.conn, "ERR wrong number of arguments for 'ECHO' command")
-        return
-    }
-    protocol.WriteBulkString(c.conn, args[1])
+	if len(args) != 2 {
+		protocol.WriteError(c.conn, "ERR wrong number of arguments for 'ECHO' command")
+		return
+	}
+	protocol.WriteBulkString(c.conn, args[1])
 }
 
 func (c *Client) set(args []string) {
-    if len(args) != 3 {
-        protocol.WriteError(c.conn, "ERR wrong number of arguments for 'SET' command")
-        return
-    }
-    key, value := args[1], args[2]
-    c.datastore.Set(key, value)
-    c.aof.AppendCommand(args)
-    protocol.WriteSimpleString(c.conn, "OK")
+	if len(args) != 3 {
+		protocol.WriteError(c.conn, "ERR wrong number of arguments for 'SET' command")
+		return
+	}
+	key, value := args[1], args[2]
+	c.datastore.Set(key, value)
+	c.aof.AppendCommand(args)
+	protocol.WriteSimpleString(c.conn, "OK")
 }
 
 func (c *Client) get(args []string) {
-    if len(args) != 2 {
-        protocol.WriteError(c.conn, "ERR wrong number of arguments for 'GET' command")
-        return
-    }
-    key := args[1]
-    value, found := c.datastore.Get(key)
-    if !found {
-        protocol.WriteNullBulkString(c.conn)
-    } else {
-        protocol.WriteBulkString(c.conn, value)
-    }
+	if len(args) != 2 {
+		protocol.WriteError(c.conn, "ERR wrong number of arguments for 'GET' command")
+		return
+	}
+	key := args[1]
+	value, found := c.datastore.Get(key)
+	if !found {
+		protocol.WriteNullBulkString(c.conn)
+	} else {
+		protocol.WriteBulkString(c.conn, value)
+	}
 }
--- internal/protocol/protocol.go
+++ internal/protocol/protocol.go
@@ -10,6 +10,7 @@ import (
     "strings"
 )
 
+// ParseRequest parses a client request from the connection
 func ParseRequest(reader *bufio.Reader) ([]string, error) {
     line, err := reader.ReadString('\n')
     if err != nil {
@@ -46,22 +47,27 @@ func ParseRequest(reader *bufio.Reader) ([]string, error) {
     return args, nil
 }
 
+// WriteSimpleString writes a simple string response to the client
 func WriteSimpleString(conn net.Conn, message string) {
     fmt.Fprintf(conn, "+%s\r\n", message)
 }
 
+// WriteError writes an error response to the client
 func WriteError(conn net.Conn, message string) {
     fmt.Fprintf(conn, "-%s\r\n", message)
 }
 
+// WriteBulkString writes a bulk string response to the client
 func WriteBulkString(conn net.Conn, message string) {
     fmt.Fprintf(conn, "$%d\r\n%s\r\n", len(message), message)
 }
 
+// WriteNullBulkString writes a null bulk string response to the client
 func WriteNullBulkString(conn net.Conn) {
     fmt.Fprint(conn, "$-1\r\n")
 }
 
+// FormatCommand formats a command for sending to the server
 func FormatCommand(args []string) string {
     var sb strings.Builder
     sb.WriteString(fmt.Sprintf("*%d\r\n", len(args)))
@@ -69,4 +75,53 @@ func FormatCommand(args []string) string {
         sb.WriteString(fmt.Sprintf("$%d\r\n%s\r\n", len(arg), arg))
     }
     return sb.String()
+}
+
+// WriteCommand sends a command to the server using the RESP protocol
+func WriteCommand(conn net.Conn, args []string) error {
+    cmd := FormatCommand(args)
+    _, err := conn.Write([]byte(cmd))
+    return err
+}
+
+// ReadResponse reads and parses the server's response
+func ReadResponse(reader *bufio.Reader) (string, error) {
+    line, err := reader.ReadString('\n')
+    if err != nil {
+        return "", err
+    }
+
+    line = strings.TrimSpace(line)
+    if len(line) == 0 {
+        return "", errors.New("empty response")
+    }
+
+    switch line[0] {
+    case '+':
+        // Simple string
+        return line[1:], nil
+    case '-':
+        // Error
+        return "(error) " + line[1:], nil
+    case ':':
+        // Integer
+        return line[1:], nil
+    case '$':
+        // Bulk string
+        length, err := strconv.Atoi(line[1:])
+        if err != nil {
+            return "", errors.New("invalid bulk string length")
+        }
+        if length == -1 {
+            return "(nil)", nil
+        }
+        buf := make([]byte, length+2) // +2 for \r\n
+        _, err = io.ReadFull(reader, buf)
+        if err != nil {
+            return "", err
+        }
+        return string(buf[:length]), nil
+    default:
+        return "", errors.New("unknown response type")
+    }
 }
